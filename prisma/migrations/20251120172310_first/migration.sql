b0aadb31ea95343057c5d3822ba9fb591d99c0ab
Kevin Derot <64579607+kevd3r@users.noreply.github.com>
2025-11-25 17:39:46 +0100

    Second

---
+++ prisma/migrations/20251120172310_first/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `badgeNumber` VARCHAR(191) NOT NULL,
+    `role` ENUM('DEV', 'ADMIN', 'USER') NOT NULL DEFAULT 'USER',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_badgeNumber_key`(`badgeNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
